cace31e09f238cc5b57808cabdbec90d52d0e879
danielmartinez4290 <dmartinez4290@gmail.com>
2020-04-27 21:57:38 -0500

    first commit

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,16 +1,16 @@
 export class InMemoryDataService {
   createDb() {
     const heroes = [
-      { id: 11, name: 'Mr. Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 11, name: 'Iron Man' },
+      { id: 12, name: 'Hulk' },
+      { id: 13, name: 'Thor' },
+      { id: 14, name: 'Flash' },
+      { id: 15, name: 'Batman' },
+      { id: 16, name: 'Wonderwoman' },
+      { id: 17, name: 'Dr Octopus' },
+      { id: 18, name: 'Venom' },
+      { id: 19, name: 'Spiderman' },
+      { id: 20, name: 'Aquaman' }
     ];
     return { heroes };
   }
